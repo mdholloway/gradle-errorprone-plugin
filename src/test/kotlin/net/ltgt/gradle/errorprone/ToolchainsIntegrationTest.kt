6cd91e4cae14c7ae700baff26377c27c1ec014a4
Thomas Broyer <t.broyer@ltgt.net>
2021-10-03 17:50:44 +0200

    Configure strong encapsulation jvm args for all 9+ JDKs

--- src/test/kotlin/net/ltgt/gradle/errorprone/ToolchainsIntegrationTest.kt
+++ src/test/kotlin/net/ltgt/gradle/errorprone/ToolchainsIntegrationTest.kt
@@ -156,7 +156,7 @@ class ToolchainsIntegrationTest : AbstractPluginIntegrationTest() {
             assertThat(result.task(":compileJava")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
             assertThat(result.output).contains(NOT_FORKED)
             assertThat(result.output).doesNotContain(JVM_ARG_BOOTCLASSPATH)
-            assertThat(result.output).doesNotContain(JVM_ARGS_STRONG_ENCAPSULATION)
+            assertThat(result.output).contains(JVM_ARGS_STRONG_ENCAPSULATION)
             // Check that the configured jvm arg is preserved
             assertThat(result.output).contains(jvmArg("-XshowSettings"))
         }
@@ -178,7 +178,7 @@ class ToolchainsIntegrationTest : AbstractPluginIntegrationTest() {
             assertThat(result.task(":compileJava")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
             assertThat(result.output).contains(FORKED)
             assertThat(result.output).doesNotContain(JVM_ARG_BOOTCLASSPATH)
-            assertThat(result.output).doesNotContain(JVM_ARGS_STRONG_ENCAPSULATION)
+            assertThat(result.output).contains(JVM_ARGS_STRONG_ENCAPSULATION)
             // Check that the configured jvm arg is preserved
             assertThat(result.output).contains(jvmArg("-XshowSettings"))
         }
